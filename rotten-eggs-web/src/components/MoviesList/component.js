8b79c8f08423c522f57071b410dd34ca77a9324d
Jumpi96 <juampilorenzo@gmail.com>
2020-09-06 22:33:56 -0300

    Filter working

--- rotten-eggs-web/src/components/MoviesList/component.js
+++ rotten-eggs-web/src/components/MoviesList/component.js
@@ -6,16 +6,24 @@ import MoviesListItem from './MoviesListItem/component';
 const MoviesList = () => {
   const [hasError, setErrors] = useState(false);
   const [movies, setMovies] = useState([]);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     fetchMovies(setMovies, setErrors)
   }, []);
-  
+
   return (
     <div>
-      <div className="row">
-        <div className="col-md">
+      <div className="form-group row">
+        <div className="col-md-6">
           <h1>Movies</h1>
+        </div>
+        <div className="col-md-6">
+          <label for="filter">Search your movie:</label>
+          <input
+            className="form-control"
+            value={filter} id="filter"
+            onChange={(e) => setFilter(e.target.value)} />
         </div>
       </div>
       <span style={{ color: "red" }}>{hasError}</span>
@@ -29,7 +37,7 @@ const MoviesList = () => {
           </tr>
         </thead>
         <tbody>
-          {movies.map(m => <MoviesListItem movie={m}/>)}
+          {movies.filter(m => m.title.includes(filter)).map(m => <MoviesListItem movie={m} />)}
         </tbody>
       </table>
     </div >
